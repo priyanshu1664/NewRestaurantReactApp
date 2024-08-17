492fa32e81e6fdcc3dd59b7939ed576c7632d768
priyanshu1664 <priyanshusingh1664@gmail.com>
2024-08-17 17:52:15 +0530

    Final Add Functionality

---
+++ src/UI/Modal.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Modal.module.css";
+import ReactDOM from "react-dom";
+import CartProvider from "../store/CartProvider";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onCloseCart} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+function Modal(props) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseCart={props.onCloseCart} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+}
+
+export default Modal;
